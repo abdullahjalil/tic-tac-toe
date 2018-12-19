98d56731e68b5f0b0158f6c493d22ed06b6b515d
Tech-a53 <tech-a53@Tech-A53s-MacBook-Air.local>
2018-12-19 17:24:59 +0000

    Fixed Black and White

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-// 8 Win conditions
-document.addEventListener("DOMContentLoaded", function(){
-  event.preventDefault()
-});
+var squares = document.getElementsByTagName('td');
+
+var turnCount = 0;
+
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", function(event){
+    if (this.innerHTML == "") {
+      if (turnCount % 2 == 0) {
+        this.classList.add("X");
+        this.innerHTML = "X";
+        turnCount++;
+      } else if (turnCount % 2 == 1) {
+        this.classList.add("O");
+        this.innerHTML = "O";
+        turnCount++;
+      }
+    }
+})
+}
